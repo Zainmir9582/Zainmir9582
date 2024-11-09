ab8c892c677fb8d36bd554876282fe8931eb0e66
Zainmir9582 <zainmir9582@gmail.com>
2024-11-10 01:59:12 +0500

    At last last

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
+// tailwind.config.js
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}', // Make sure your pages folder is included
+    './components/**/*.{js,ts,jsx,tsx}', // Include your components folder
+    './app/**/*.{js,ts,jsx,tsx}', // If you're using the `app` directory in Next.js 13+
   ],
   theme: {
     extend: {},
